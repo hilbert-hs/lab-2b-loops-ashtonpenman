a764068049a46b408716199d7de0bd3f3e579d2d
ashtonpenman <88688089+ashtonpenman@users.noreply.github.com>
2025-11-21 17:19:47 +0000

    done

--- Grades.java
+++ Grades.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
-
-public static void main(String[] args) {
+public class Grades {
+  public static void main(String[] args) {
   //  write your code here
+
   
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please put the amount of grades that you will be entering.");
+    int end = scan.nextInt();
+    double Total = 0;
+    for(int i = 0; i < end; i++){
+      System.out.println("Please enter one grade at a time.");
+      Total += scan.nextDouble();
+
+    }
+    double average = Total / end;
+    System.out.println("Your average is: " + average);
+
+    scan.close();
+  }
 }
+  
+
 
 // write any helper methods here
--- Tickets.java
+++ Tickets.java
@@ -1,8 +1,43 @@
 import java.util.Scanner;
 
-public static void main(String[] args) {
+public class Tickets {
+  public static void main(String[] args) {
   // write your code here
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Please choose one seat: B for box, P for pavilion, and L for lawn. Press Q to quit.");
+  int totalB = 0;
+  int totalP = 0;
+  int totalL = 0;
+  double costB = 0;
+  double costP = 0;
+  double costL = 0;
+  double conv = 0;
+  String option = scan.nextLine();
+  while(!option.toLowerCase().equals("q")){
+    System.out.println("Please pick another option.");
+    option = scan.nextLine();
+    if(option.toLowerCase().equals("b")){
+      totalB++;
+      costB += 75;
+      conv += 1.5;
+    }
+    else if(option.toLowerCase().equals("p")){
+      totalP++;
+      costP += 30;
+      conv += 1.5;
+    }
+    else if(option.toLowerCase().equals("l")){
+      totalL++;
+      costL += 21;
+      conv += 1.5;
+    }
 
+  }
+  System.out.println(totalB + "   Box tickets:       $" + costB);
+  System.out.println(totalP + "   Pavilion tickets:  $" + costP);
+  System.out.println(totalL + "   Lawn tickets:      $" + costL);
+  System.out.println("    Convenience fee:    $" + conv);
+  System.out.println("    total:              $" + (conv + costL + costP + costB));
+  }
 }
-
 // write any helper methods here
